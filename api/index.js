2c27423e890be51e2abad0d5fcbfe719de489806
irishabhnegi <rishabhnegi175@gmail.com>
2024-03-17 18:33:37 +0530

    add a middleware and a function to handler errors

--- api/index.js
+++ api/index.js
@@ -19,6 +19,16 @@ app.use(express.json());
 app.use('/api/user', userRoutes);
 app.use('/api/auth', authRoutes);
 
+app.use((err, req, res, next) => {
+  const statusCode = err.statusCode || 500;
+  const message = err.message || 'Internal Server Error';
+  return res.status(statusCode).json({
+    success: false,
+    message,
+    statusCode,
+  });
+});
+
 app.listen(3000, () => {
   console.log('Server listening on port 3000');
 });
